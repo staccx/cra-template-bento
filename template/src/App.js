95fee5f2df525a64cf1ad8c57e444b894c7d5ed8
Espen Th Sæverud <espensaeverud@gmail.com>
2020-02-27 20:26:03 +0100

    feat: updated to use new ThemeProvider

--- template/src/App.js
+++ template/src/App.js
@@ -1,12 +1,10 @@
 import React from "react"
 import { ThemeProvider } from "styled-components"
-import { Button, CheckBox } from "@staccx/bento"
-import baseTheme from "@staccx/bento/lib/theming/baseTheme"
-
+import { Button, CheckBox, ThemeProvider } from "@staccx/bento"
 
 function App () {
   return (
-    <ThemeProvider theme={baseTheme}>
+    <ThemeProvider>
       <Button onClick={() => console.log("Something")}>This is a button</Button>
       <CheckBox id={"test"}>Tester</CheckBox>
     </ThemeProvider>
