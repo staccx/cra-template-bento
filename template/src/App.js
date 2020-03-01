c0637737ecf444083bfa64b78cbf287d5956f8c4
Espen Th Sæverud <espensaeverud@gmail.com>
2020-03-01 12:49:23 +0100

    feat: extended template to include variant building

--- template/src/App.js
+++ template/src/App.js
@@ -1,11 +1,14 @@
 import React from "react"
-import { Button, CheckBox, ThemeProvider } from "@staccx/bento"
+import { Button, ThemeProvider } from "@staccx/bento"
 
 function App () {
   return (
     <ThemeProvider>
-      <Button onClick={() => console.log("Something")}>This is a button</Button>
-      <CheckBox id={"test"}>Tester</CheckBox>
+      <Button onClick={() => console.log("Something")}>Default</Button>
+      <Button variant={"primary"} onClick={() => console.log("Something")}>Primary</Button>
+      <Button variant={"secondary"} onClick={() => console.log("Something")}>Secondary</Button>
+      <Button variant={"tertiary"} onClick={() => console.log("Something")}>Tertiary</Button>
+      <Button variant={["tertiary", "dotted"]} onClick={() => console.log("Something")}>Multiple variants</Button>
     </ThemeProvider>
   )
 }
---
+++ template/src/theme/variants/Button.variants.js
@@ -0,0 +1,28 @@
+import {Button, theming} from "@staccx/bento"
+import {css} from "styled-components"
+
+/**
+ * With no seconda argument will write to default theme
+ */
+Button.createVariants({
+  button: {
+    [theming.VARIANT_DEFAULT]: css`border-radius: 15px;`,
+    primary: css`background-color: cornflowerblue;`,
+    secondary: css`background-color: coral;`,
+    tertiary: css`background-color: crimson;`,
+    dotted: css`border: 1px dotted black;`
+  }
+})
+
+/**
+ * Use the second argument to createVariants on another theme
+ */
+Button.createVariants({
+  button: {
+    [theming.VARIANT_DEFAULT]: css`border-radius: 15px; color: #e1e1e1;`,
+    primary: css`background-color: cornflowerblue;`,
+    secondary: css`background-color: coral;`,
+    tertiary: css`background-color: crimson;`,
+    dotted: css`border: 1px dotted black;`
+  }
+}, "dark")
